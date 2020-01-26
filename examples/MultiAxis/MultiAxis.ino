52cb065154b9177de563d94ea49b97d9c4e1a159
NaotoFujihiro <naoto.fujihiro@gmail.com>
2020-01-26 19:25:52 +0900

    Enable to build the source

--- examples/MultiAxis/MultiAxis.ino
+++ examples/MultiAxis/MultiAxis.ino
@@ -10,19 +10,25 @@
  * A copy of this license has been included with this distribution in the file LICENSE.
  */
 #include <Arduino.h>
-#include <pin_config.h>
+#include "pin_config.h"
+#include "motor_commands.h"
+#include "../../TureLibrary/src/utils.h"
+
 #include "BasicStepperDriver.h"
 #include "MultiDriver.h"
 #include "SyncDriver.h"
 
 // Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
 #define MOTOR_STEPS 200
+
 // Target RPM for X axis motor
 // #define MOTOR_X_RPM MAX_RPM
 #define MOTOR_X_RPM 120
+
 // Target RPM for Y axis motor
 // #define MOTOR_Y_RPM MAX_RPM
 #define MOTOR_Y_RPM 120
+
 // // Target RPM for Z axis motor
 // #define MOTOR_Z_RPM MAX_RPM
 
@@ -35,8 +41,8 @@
 #define STEP_Y      PIN_5_STEP
 
 // // Z motor
-// #define DIR_Z       PIN_5_DIR
-// #define STEP_Z      PIN_5_STEP
+// #define DIR_Z       PIN_2_DIR
+// #define STEP_Z      PIN_2_STEP
 
 // If microstepping is set externally, make sure this matches the selected mode
 // 1=full step, 2=half step etc.
@@ -50,14 +56,18 @@ BasicStepperDriver stepperY(MOTOR_STEPS, DIR_Y, STEP_Y);
 
 // Pick one of the two controllers below
 // each motor moves independently, trajectory is a hockey stick
-// MultiDriver controller(stepperX, stepperY);
+MultiDriver controller(stepperX, stepperY);
+// MultiDriver controller(stepperX, stepperY, stepperZ);
 // OR
 // synchronized move, trajectory is a straight line
+// SyncDriver controller(stepperX, stepperY);
 // SyncDriver controller(stepperX, stepperY, stepperZ);
-SyncDriver controller(stepperX, stepperY);
+
 
 void setup() {
+    Serial.begin(115200);
     delay(4000);
+
     /*
      * Set target motors RPM.
      */
@@ -72,11 +82,6 @@ void setup() {
 }
 
 void loop() {
-
-    controller.rotate(-1080, -1080);
+    controller.rotate(convertToSubDegree(double(MOTOR_0_MOVE_DISTANCE_CMD)), convertToSubDegree(double(MOTOR_1_MOVE_DISTANCE_CMD)));
     delay(15000);
-    // controller.rotate(-90*5, -30*15);
-    // delay(1000);
-    // controller.rotate(0, -30*15);
-    // delay(30000);
 }
